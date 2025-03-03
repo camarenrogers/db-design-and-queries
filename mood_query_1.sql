2e72d45c89c84012e6064cc4b907917d1b423956
Camaren Rogers <159051616+camarenrogers@users.noreply.github.com>
2025-03-03 14:38:48 +0000

    Delete mood_query_1.sql

--- mood_query_1.sql
+++
@@ -1,5 +0,0 @@
-SELECT s.title, s.artist, l.listen_date
-FROM Listening_History l
-JOIN Songs s ON l.song_id = s.song_id
-JOIN Moods m ON l.mood_id = m.mood_id
-WHERE l.user_id = ? AND m.mood_name = ?
